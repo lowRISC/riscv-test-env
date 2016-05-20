9c9111069e2902e65b0ab072ff680d2e4feedfc2
Wei Song <wsong83@gmail.com>
2016-05-20 15:47:12 +0100

    replace tohost with our behav host

--- encoding.h
+++ encoding.h
@@ -71,8 +71,8 @@
 #define DEFAULT_NMIVEC     0x00001004
 #define DEFAULT_MTVEC      0x00001010
 #define CONFIG_STRING_ADDR 0x0000100C
-#define EXT_IO_BASE        0x40000000
-#define DRAM_BASE          0x80000000
+#define DRAM_BASE          0x40000000
+#define HOST_BASE          0x00004000
 
 // page table entry (PTE) fields
 #define PTE_V     0x001 // Valid
