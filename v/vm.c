737535de33ffeed24eb09fcbb41f680c9829825f
Alex Bradbury <asb@asbradbury.org>
2017-03-01 10:33:51 +0000

    Revert "allow access to tohost from kernel virtual space"
    This reverts commit 0097da97d051bab6558614416f2c6e75e28e80ec.
    
    Conflicts:
    	v/vm.c

--- v/vm.c
+++ v/vm.c
@@ -9,19 +9,19 @@
 void trap_entry();
 void pop_tf(trapframe_t*);
 
-#define pa2kva(pa) ((void*)(pa) - DRAM_BASE - MEGAPAGE_SIZE)
-#define uva2kva(pa) ((void*)(pa) - MEGAPAGE_SIZE)
-
-volatile uint64_t *tohost = ((uint64_t *)((MAX_TEST_PAGES << PGSHIFT) - MEGAPAGE_SIZE));
-volatile uint64_t *fromhost = ((uint64_t *)((MAX_TEST_PAGES << PGSHIFT) - MEGAPAGE_SIZE));
+volatile uint64_t tohost __attribute__((aligned(64)));
+volatile uint64_t fromhost __attribute__((aligned(64)));
 
 static void do_tohost(uint64_t tohost_value)
 {
-  *tohost = tohost_value;
-  while (*fromhost == 0)
+  tohost = tohost_value;
+  while (fromhost == 0)
     ;
-  *fromhost = 0;
+  fromhost = 0;
 }
+
+#define pa2kva(pa) ((void*)(pa) - DRAM_BASE - MEGAPAGE_SIZE)
+#define uva2kva(pa) ((void*)(pa) - MEGAPAGE_SIZE)
 
 static uint64_t lfsr63(uint64_t x)
 {
@@ -229,7 +229,6 @@ void vm_boot(long test_addr, long seed)
     kernel_l3pt[i] = ((i + DRAM_BASE/RISCV_PGSIZE) << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W | PTE_X;
   }
   freelist_nodes[MAX_TEST_PAGES-1].next = 0;
-  kernel_l3pt[MAX_TEST_PAGES] = ((pte_t)HOST_BASE >> PGSHIFT << PTE_PPN_SHIFT) | PTE_V | PTE_R | PTE_W; // map host to the last page
 
   trapframe_t tf;
   memset(&tf, 0, sizeof(tf));
